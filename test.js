ccbb3d3e8d15731e898a3dc0d439a46406f4690b
Youngpro95 <mmyoung95@naver.com>
2022-07-27 05:23:51 +0900

    feat : 검색창 에서 최근검색어 5개 넘어가면 삭제되게끔 함

--- test.js
+++ test.js
@@ -1,19 +1,52 @@
-const inputVal = document.body.querySelector('input')
-const searchButton = document.body.querySelector('body > button')
+const inputVal = document.body.querySelector("input");
+const searchButton = document.body.querySelector("body > button");
 
 console.log(inputVal.value);
 let searchArray = [];
 
-inputVal.addEventListener('keydown', (e)=>{
-  if(e.key==="Enter"){
-    // console.log(e.target.value);
-    // searchArray.push(e.target.value);
+function addDiv() {
+  let keywordHistory = document.querySelector("#recentKeyword");
+  let searchHistoryDiv = document.createElement("div");
+  searchHistoryDiv.innerHTML = inputVal.value;
+  searchHistoryDiv.setAttribute("id", "searchHistory");
+
+  if (searchArray.length === 6) {
+    (function () {
+      const dom = document.querySelector("#searchHistory:nth-child(5)");
+      let p = dom.parentElement;
+      p.removeChild(dom);
+      console.log("실행");
+      console.log(dom);
+    })();
+  }
+  keywordHistory.prepend(searchHistoryDiv);
+}
+
+inputVal.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
     searchArray.unshift(e.target.value);
-    if(searchArray.length === 6){
+    addDiv();
+    if (searchArray.length === 6) {
       searchArray.pop();
-      console.log(searchArray)
-    }else{
-      console.log(searchArray)
+      console.log(searchArray);
+    } else {
+      console.log(searchArray);
     }
   }
-}) // 검색 히스토리 배열 저장 및 5개이상 시 가장 처음 검색했던 내용 삭제
+}); // 검색 히스토리 배열 저장 및 5개이상 시 가장 처음 검색했던 내용 삭제
+
+inputVal.addEventListener("click", (e) => {
+  console.log("눌렸습니다");
+  // const keywordDiv = document.body.querySelector("#recentKeyword");
+  // console.log(keywordDiv)
+  // if (keywordDiv.style.visibility === "hidden") {
+  //   keywordDiv.style.visibility = "visible";
+  // } else {
+  //   keywordDiv.style.visibility = "hidden";
+  // }
+});
+
+inputVal.addEventListener("compositionupdate", (e) => {
+  //keyup 후보
+  setTimeout;
+});
